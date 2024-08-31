ebac535f483d2615bd49163484f8666034f49bd9
it22092274 <128246276+it22092274@users.noreply.github.com>
2024-08-31 14:30:31 +0530

    awasama commituwa
    onna dammo

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Change '/api' to the path you want to proxy
+      '/api': {
+        target: 'http://localhost:5000/api', // Change this to your backend server's URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
