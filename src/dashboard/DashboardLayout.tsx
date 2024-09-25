beb960a882a1f4980728f433ae6fddb74a2b66cf
Rusiru Kalendra De Silva <IT22927316@my.sliit.lk>
2024-09-25 10:00:13 +0530

    මා ඉතින් යන්න යනවා

--- src/dashboard/DashboardLayout.tsx
+++ src/dashboard/DashboardLayout.tsx
@@ -1,17 +1,16 @@
 import { Link } from 'react-router-dom';
-import { AiFillHome, AiOutlineUser, AiOutlineRise, AiOutlineFileDone, AiOutlineTeam, AiOutlineUserSwitch, AiTwotoneFund, AiTwotoneHeart, AiOutlineForm, AiOutlineQuestion, AiOutlineShopping} from 'react-icons/ai';
+import { AiFillHome, AiOutlineUser, AiOutlineRise, AiOutlineFileDone, AiOutlineTeam, AiOutlineUserSwitch, AiTwotoneFund, AiTwotoneHeart, AiOutlineForm, AiOutlineQuestion, AiOutlineShopping } from 'react-icons/ai';
 import { MdProductionQuantityLimits } from 'react-icons/md';
 
-import adminimg from '../assets/vihangaimg.jpg'
-import addproductimg from '../assets/addproductimg.jpg'
-import manageimg from '../assets/manageimg.jpg'
-import productstatsimg from '../assets/statisticsimg.jpg'
-
-import managereviewimg from '../assets/managereviewimg12.jpg'
-import reviewstatimg from '../assets/reviewstatimg12.jpg'
+import adminimg from '../assets/vihangaimg.jpg';
 import faqimg from '../assets/faqimg12.jpg'
+import managereviewimg from '../assets/managerev12.jpg'
+import reviewstatsimg from '../assets/revstat12.jpg'
+import financereportimg from '../assets/reportfinanceimg.jpg'
+import manageorderimg from '../assets/ordermanageimg.jpg'
 
 const DashboardLayout: React.FC = ({}) => {
+  
   return (
     <div className="dashboard-layout">
       {/* Sidebar */}
@@ -26,7 +25,7 @@ const DashboardLayout: React.FC = ({}) => {
                 <AiFillHome /> Dashboard
               </Link>
             </li>
-            <hr/>
+            <hr />
             <li>
               <Link to="">
                 <AiOutlineForm /> Add Products
@@ -47,7 +46,7 @@ const DashboardLayout: React.FC = ({}) => {
                 <AiTwotoneFund /> Product Statistics
               </Link>
             </li>
-            <hr/>
+            <hr />
             <li>
               <Link to="">
                 <AiOutlineShopping /> Manage Orders
@@ -58,7 +57,7 @@ const DashboardLayout: React.FC = ({}) => {
                 <AiOutlineFileDone /> Finance Report
               </Link>
             </li>
-            <hr/>
+            <hr />
             <li>
               <Link to="/admin/manage-reviews">
                 <AiTwotoneHeart /> Manage Reviews
@@ -69,16 +68,16 @@ const DashboardLayout: React.FC = ({}) => {
                 <AiTwotoneFund /> Reviews Statistics
               </Link>
             </li>
-            <hr/>
+            <hr />
             <li>
               <Link to="/admin/manage-faq">
                 <AiOutlineQuestion /> Manage FAQs
               </Link>
             </li>
-            <hr/>
+            <hr />
             <li>
               <Link to="">
-                <AiOutlineTeam  /> Manage User Profiles
+                <AiOutlineTeam /> Manage User Profiles
               </Link>
             </li>
             <li>
@@ -96,112 +95,153 @@ const DashboardLayout: React.FC = ({}) => {
         </nav>
       </aside>
 
-      
+      {/* PALAWENI KAAALLA */}
       <main className="main-content">
-        
         <header className="header">
           <div className="header-left">
-            <img src={adminimg} className="profile-image" />
+            <img src={adminimg} className="profile-image" alt="Admin" />
             <div className="user-info">
-                {/* Methana hari ewa dapan */}
-              <h2 className='text-xl text-bold'>Naughty Vihanga Max</h2>
-              <p>System Administrator</p>
+              <h2 className="text-xl font-bold">Nuaghty Vihanga Max</h2>
+              <p>System Adminsitrator</p>
             </div>
           </div>
           <div className="header-right">
-            <p className="date">27 Feb</p>
-            <input type="text" className="search" placeholder="Search" />
-            <button className="notification-button">
-              <span className="notification-dot"></span>
-            </button>
-            <button className="message-button"></button>
+            <p>searchbar</p>
+            <button>icon2</button>
+            <button>icon1</button>
           </div>
         </header>
 
-        
-        <div className="content-area">
 
-            
-            <section className="analytics">
+        <div className="dashboard-container">
+        <h2 className='text-xl font-bold leading-snug text-black'>Monthly Sales Dashboard</h2>
+        <section className="dashboard-overview">
+          <div className="stat-card">
+            <h3>Net Sales For This Month</h3>
+            <p className='text-2xl font-bold text--800'>132,324LKR</p>
+          </div>
 
-                <div className="product-section">
-                    <img src={addproductimg} alt="Add Products" className="product-image" />
-                    <div className="content">
-                        <h3>Add Products</h3>
-                        <p>Add new arrivals and stock products</p>
-                        <button className="navigate-button">Add Products</button>
+          <div className="stat-card">
+            <h3>Number Of Total Sold Products</h3>
+            <p className='text-2xl font-bold text-green-800'>1304</p>
+          </div>
+        </section>
+
+        {/* dewani KAAALLA */}
+        <h2 className='text-xl font-bold leading-snug text-black mt-5'>Products Dashboard</h2>
+          <div className='flex w-full md:flex-row  items-center gap-5'>
+              {/* left side */}
+              <div className='md:w-1/2'>
+                <section className="dashboard-overview">
+                    <div className="stat-card">
+                      <h3 className='text-xl font-semibold text-gray-700 text-left'>Add a product</h3>
+                      <p className='text-left'>Upload new products to the catalog.</p>
+                      <button className='bg-blue-800 text-white font-semibold px-5 py-2 rounded hover:bg-black  transition-all duration-300'>
+                        <AiOutlineForm />
+                      </button>
                     </div>
-                </div>
 
-                <div className="product-section">
-                    <img src={manageimg} alt="Add Products" className="product-image" />
-                    <div className="content">
-                        <h3>Manage Products</h3>
-                        <p>Manage product stocks and keep track of the products</p>
-                        <button className="navigate-button">Manage Products</button>
+                    <div className="stat-card">
+                      <h3 className='text-xl font-semibold text-gray-700 text-left'>Manage Products</h3>
+                      <p className='text-left'>View items, update or remove products.</p>
+                      <button className='bg-blue-800 text-white font-semibold px-5 py-2 rounded hover:bg-black  transition-all duration-300'>
+                      <MdProductionQuantityLimits />
+                      </button>
                     </div>
-                </div>
 
-                <div className="product-section">
-                    <img src={productstatsimg} alt="Add Products" className="product-image" />
-                    <div className="content">
-                        <h3>Statistics</h3>
-                        <p>Check product statistics to see current best sellers and others</p>
-                        <button className="navigate-button">Check Statistics</button>
+                    <div className="stat-card">
+                      <h3 className='text-xl font-semibold text-gray-700 text-left'>Product Statistics</h3>
+                      <p className='text-left'>Access key insights and data on product performance.</p>
+                      <button className='bg-blue-800 text-white font-semibold px-5 py-2 rounded hover:bg-black transition-all duration-300'>
+                      <AiTwotoneFund />
+                      </button>
                     </div>
-                </div>
-            </section>
-            
-            {/* Add orders section */}
-            <section className="analytics">
-                <div className="product-section">
-                    <div className="content">
-                        <h3>Manage Orders</h3>
-                        <p>Manage user purchases and user current orders here!</p>
-                        <button className="navigate-button">Check Orders</button>
-                    </div>
-                </div>
 
-                <div className="product-section">
-                    <div className="content">
-                        <h3>Financial Reports</h3>
-                        <p>Generate financial reports based on user purchases!</p>
-                        <button className="navigate-button">Check Reports</button>
+                    <div className="stat-card">
+                      <h3 className='text-xl font-semibold text-gray-700 text-left'>Product Reports</h3>
+                      <p className='text-left'>Generate detailed reports on products.</p>
+                      <button className='bg-blue-800 text-white font-semibold px-5 py-2 rounded hover:bg-black  transition-all duration-300'>
+                      <AiOutlineFileDone />
+                      </button>
                     </div>
-                </div>
-            </section>
+                </section>
+              </div>
+
+            {/* Right Side */}
+              <div className='md:w-1/2'>
+                <section>
+                        <div className="chart">
+                          <h3>Mokak hari chart ekak methanata dapan - PRODCUTS SAMBANDAWA HABAI</h3>
+                          <canvas id="recentMovementChart"></canvas>
+                        </div>
+                </section>
+              </div>
+          </div>
 
 
-            {/* Add reviews section */}
-            <section className="analytics">
-            <div className="product-section">
-            <img src={managereviewimg} alt="Add Products" className="product-image" />
-                    <div className="content">
-                        <h3>Manage Reviews</h3>
-                        <p>Manage and approve user submitted reviews</p>
-                        <button className="navigate-button">User Reivews</button>
+          {/* thunweni KAAALLA */}
+          <div className='flex w-full md:flex-row  items-center gap-5'>
+              <div>
+              <h2 className='text-xl font-bold leading-snug text-black mt-5'>Reviews & FAQs dashboard</h2>
+                <section className="dashboard-overview-2">
+                    <div className="stat-card">
+                      <h3 className='text-xl font-semibold text-black'>Manage FAQs</h3>
+                      <img src={faqimg} />
+                      <p className='mt-2 '>Oversee and update frequently asked questions to ensure users have accurate information.</p>
+                      <button className='bg-black  font-semibold text-white mt-5 px-10 py-2 rounded hover:bg-yellow-400  transition-all duration-300'>
+                      Check FAQs
+                      </button>
                     </div>
-                </div>
 
-                <div className="product-section">
-                <img src={reviewstatimg} alt="Add Products" className="product-image" />
-                    <div className="content">
-                        <h3>Review Statistics</h3>
-                        <p>Generate reports based user submitted reviews on our products</p>
-                        <button className="navigate-button">Check Statistics</button>
+                    <div className="stat-card">
+                      <h3 className='text-xl font-semibold text-black'>Manage Reviews</h3>
+                      <img src={managereviewimg} />
+                      <p className='mt-2 '>Monitor and manage product reviews, including editing and deleting as needed.</p>
+                      <button className='bg-black  font-semibold text-white mt-5 px-10 py-2 rounded hover:bg-blue-500  transition-all duration-300'>
+                      Check Reviews
+                      </button>
                     </div>
-                </div>
 
-                <div className="product-section">
-                <img src={faqimg} alt="Add Products" className="product-image" />
-                    <div className="content">
-                        <h3>Manage FAQs</h3>
-                        <p>Manage user frequently asked questions here and answer them!</p>
-                        <button className="navigate-button">Manage FAQs</button>
+                    <div className="stat-card">
+                      <h3 className='text-xl font-semibold text-black'>Review Statistics</h3>
+                      <img src={reviewstatsimg} />
+                      <p className='mt-2 '>Analyze customer feedback  with insights on ratings and averages for selected periods.</p>
+                      <button className='bg-black  font-semibold text-white mt-5 px-10 py-2 rounded hover:bg-purple-800  transition-all duration-300'>
+                      Check Statistics
+                      </button>
                     </div>
+
+                </section>
+
+
+                {/* 4weni KAAALLA */}
+                <h2 className='text-xl font-bold leading-snug text-black mt-5'>Order Status Dashboard</h2>
+                <section className="dashboard-overview">
+                <div className="stat-card flex items-center">
+                  <img src={manageorderimg} className="w-1/3" />
+                  <div className="ml-5">
+                    <h3 className='text-xl font-semibold text-black text-left'>Manage Orders</h3>
+                    <p className='mt-2 text-left'>Easily view, track, and update customer orders, ensuring smooth order fulfillment and timely delivery.</p>
+                    <button className='bg-black font-semibold text-white mt-5 px-10 py-2 rounded hover:bg-blue-500 transition-all duration-300'>
+                      Check Orders
+                    </button>
+                  </div>
                 </div>
-            </section>
-        </div>
+                <div className="stat-card flex items-center">
+                  <img src={financereportimg} className="w-1/3" />
+                  <div className="ml-5">
+                    <h3 className='text-xl font-semibold text-black text-left'>Finance Reports</h3>
+                    <p className='mt-2 text-left'>Access detailed financial summaries, including revenue, expenses, and profit trends, to help manage your business's financial health.</p>
+                    <button className='bg-black font-semibold text-white mt-5 px-10 py-2 rounded hover:bg-green-300 transition-all duration-300'>
+                      Check Reports
+                    </button>
+                  </div>
+                </div>
+                </section>
+
+              </div>
+            </div>
+          </div>
       </main>
     </div>
   );
