4bac70b67c09d8c4ceab157ec760c10035a73bc5
Rusiru Kalendra De Silva <IT22927316@my.sliit.lk>
2024-10-04 09:59:29 +0530

    UI changes kara chutti pataw ne
    Navbar - done
    Logo changes
    Swiper changes
    link changes

--- src/pages/banner/BannerCard.tsx
+++ src/pages/banner/BannerCard.tsx
@@ -4,34 +4,43 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-cards';
+import 'swiper/css/effect-creative';
 
 import './BannerCard.css';
 
 // import required modules
-import { EffectCards } from 'swiper/modules';
+import { EffectCreative } from 'swiper/modules';
 
-
-const BannerCard :React.FC= () => {
+export default function App() {
   return (
+    <>
     <div className='banner'>
-        <Swiper
-        effect={'cards'}
+      <Swiper
         grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper">
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
-            <SwiperSlide></SwiperSlide>
+        effect={'creative'}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+          },
+        }}
+        modules={[EffectCreative]}
+        className="mySwiper"
+      >
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
       </Swiper>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
-
-export default BannerCard
